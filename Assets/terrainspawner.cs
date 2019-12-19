67f893960bd8ec28bbe20305da142f089604ec3c
EdwardBurden <43952989+EdwardBurden@users.noreply.github.com>
2019-12-19 15:07:28 +0000

    Terrain
    Created simple terrain, spawned it from prefab.Checked collisons worked with it

---
+++ Assets/terrainspawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class terrainspawner : MonoBehaviour
+{
+
+    public GameObject Terrain;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Instantiate(Terrain);
+            this.gameObject.AddComponent<Rigidbody>();
+        }
+    }
+}
